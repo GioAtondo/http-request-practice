0c740b03f4049e24d76bf9b0b252018a527f5f37
Giovanni Alberto Salas <gioatondo7@gmail.com>
2023-05-30 10:09:09 -0700

    Accessing Services

--- wsearch/src/app/app.component.spec.ts
+++ wsearch/src/app/app.component.spec.ts
@@ -17,7 +17,6 @@ describe('AppComponent', () => {
   it(`should have as title 'wsearch'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('wsearch');
   });
 
   it('should render title', () => {
