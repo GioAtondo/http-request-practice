b650f873f17c014f75560b85f8d5b1a0e43aecb3
Giovanni Alberto Salas <gioatondo7@gmail.com>
2023-05-29 17:49:13 -0700

    Handling form submission

--- wsearch/src/app/search-bar/search-bar.component.ts
+++ wsearch/src/app/search-bar/search-bar.component.ts
@@ -1,10 +1,27 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-search-bar',
   templateUrl: './search-bar.component.html',
   styleUrls: ['./search-bar.component.css']
 })
-export class SearchBarComponent {
+export class SearchBarComponent implements OnInit{
+  term = '';
 
+  constructor(){
+
+  }
+
+  ngOnInit(){
+
+  }
+
+  onInput(value: string){
+    this.term = value;
+  }
+
+  onFormSubmit(event: any){
+    event.preventDefault();
+    console.log(this.term);
+  }
 }
